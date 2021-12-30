dcf02684de6ba41c1ddd41ea4dd459683ba74333
ingennuis <bastien.marc35@gmail.com>
2021-12-30 14:33:41 +0100

    fonctions ask_dir replace_space

--- Test.py
+++ Test.py
@@ -1,1 +1,24 @@
-coucou c'est moi je fais coucou haha!
+#!/bin/python3
+import os
+
+yes_answer=["o", "y", "oui", "yes"]
+space_to=['-', '_', '']
+
+
+def ask_dir():
+    place=os.getcwd()
+    choix_rep=str(input(f'Votre position :{place}\n Voulez-vous changer de répertoire [O/N] ?'))
+    if choix_rep.lower() in yes_answer:
+        location = str(input("Spécifiez le répertoire voulu :"))
+    else :
+        location=os.getcwd()
+    return location
+
+def replace_space(name, replace_caracter):
+    name = name.replace(' ', replace_caracter)
+    return name
+
+name=str(input('name :'))
+i=str(input(f'Remplacer les espaces dans les noms par : \n'for signe in space_to[]:'{signe}\n'))
+
+print(replace_space(name, space_to[i]))
